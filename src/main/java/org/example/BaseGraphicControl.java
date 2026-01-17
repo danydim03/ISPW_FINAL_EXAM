65bdeda0df170ba435244217765b6f3a06a0045a
danydim03 <151035632+danydim03@users.noreply.github.com>
2026-01-17 10:21:26 +0100

    feat: Add imports for UI components, message enums, and common utility classes.

--- src/main/java/org/example/BaseGraphicControl.java
+++ src/main/java/org/example/BaseGraphicControl.java
@@ -1,15 +1,23 @@
 package org.example;
 
-
+import org.example.enums.ExceptionMessagesEnum;
+import org.example.enums.UserErrorMessagesEnum;
 import org.example.exceptions.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class BaseGraphicControl implements Initializable {
@@ -59,8 +67,6 @@ public class BaseGraphicControl implements Initializable {
         if (content.getChildren().size() >= 2)
             backButton.setVisible(true);
     }
-
-
 
     void openMainPage(Node node, String name) {
         content.getChildren().clear();
